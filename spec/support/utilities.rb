e9f94c274ae1a758b022c02bde57859d752b098c
Nhat Tan <nhattan2012@gmail.com>
2014-01-13 11:32:38 +0700

    Finish user edit, update, index, and destroy actions

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -9,6 +9,7 @@ def full_title(page_title)
   end
 end
 
+# Chapter 8
 def valid_signin(user)
 	fill_in "Email", with: user.email.upcase
 	fill_in "Password", with: user.password
@@ -18,5 +19,20 @@ end
 RSpec::Matchers.define :have_error_message do |message|
 	match do |page|
 		expect(page).to have_selector('div.alert.alert-error', text: message) 
+	end
+end
+
+# Chapter 9
+def sign_in(user, options={})
+	if options[:no_cabybara]
+		# Sign in when not using Cabybara.
+		remember_token = User.new_remember_token
+		cookies[:remember_token] = remember_token
+		user.update_attribute(:remember_token, User.encrypt(remember_token))
+	else
+		visit signin_path
+		fill_in "Email", with: user.email
+		fill_in "Password", with: user.password
+		click_button "Sign in"
 	end
 end
